6e7e1ea78220cf9b5f89e1969fd2ba7a40ed86ee
Rafael Aybar Segura <ras212@users.noreply.github.com>
2016-07-14 20:19:54 +0200

    Update Practica carpetas.py

--- Practica carpetas.py
+++ Practica carpetas.py
@@ -3,15 +3,13 @@ def rename_files ():
     # 1) Vamos a obtener el nombre de los archivos
    file_list = os.listdir ("D:\prank")
    print (file_list)
-    saved_path = os.getcwd ()
+   saved_path = os.getcwd ()
     
    
-   print ("El directorio activo es " +saved_path)
+   print ("El directorio activo es " +str(saved_path))
    os.chdir("D:\prank")
   
     # 2) Hacemos un bucle que los renombrará uno a uno:
    for file_name in file_list :
       os.rename (file_name, file_name.translate (None, "0123456789"))
       os.chdir(saved_path)
-       rename_files()
-    
